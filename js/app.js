65363a010a3841c5e3637563beec3cde51b93375
Dayana Souza <dayanalvesouza@gmail.com>
2024-03-12 17:01:19 -0300

    Criado função alterarStatus

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
-function alterarStatus() {
-    let botao = document.getElementById('')
-    if (botao.classList) {
-        
+function alterarStatus(id) {
+    let botao = document
+        .getElementById("game-${id}")
+        .querySelector(".dashboard__item__button");
+
+    if (botao.textContent === "Alugar") {
+        botao.textContent = "Devolver";
+        botao.classList.add("dashboard__item__button--return");
+        botao.parentElement
+            .querySelector(".dashboard__item__img")
+            .classList.add("dashboard__item__img--rented");
     } else {
-        
+        botao.textContent = "Alugar";
+botao.classList.remove("dashboard__item__button--return");
+botao.parentElement.querySelector('.dashboard__item__img').classList.remove('dashboard__item__img--rented');
     }
 }
